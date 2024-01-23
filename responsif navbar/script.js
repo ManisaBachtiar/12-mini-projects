4c93c1c6ffe21b41082c5bdea1229c9fb293d799
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-01-23 14:36:05 +0700

    memperbaiki kesalahan

--- responsif navbar/script.js
+++ responsif navbar/script.js
@@ -1,22 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("btn");
-    const tam = document.getElementById("tam");
-    tam.classList.add('none');
-    btn.addEventListener("click", function () {
-      tam.classList.toggle("none");
-      
-    });
-    
-    var laptop = window.matchMedia("(min-width: 601px)")
-    
-    window.onresize = () => {
-      if(laptop.matches){
-       console.log("AKU DI DESKTOP")
-        tam.classList.remove("none")
-      } else {
-        console.log("AKU DI MOBILE")
-        tam.classList.add("none")
-      }; 
-    }
-    
+  const btn = document.getElementById("btn");
+  const tam = document.getElementById("tam");
+
+  tam.classList.add("none");
+  btn.addEventListener("click", function () {
+    tam.classList.toggle("flex");
   });
+});
