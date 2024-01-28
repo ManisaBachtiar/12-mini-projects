8ee317e5993750cf328028b799ec791a204e5a12
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-01-28 22:43:38 +0700

    adding a little style

--- navigation/script.js
+++ navigation/script.js
@@ -1,19 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("btn");
-    const tam = document.getElementById("tam");
-    btn.addEventListener("click", function () {
-        
-            tam.classList.toggle("show");
-        
-      let laptop = window.matchMedia("(max-width: 601px)");
-      window.onresize = () => {
-        if (laptop.matches) {
-          tam.classList.toggle("show");
-        } else {
-          tam.classList.remove("show");
-        }
-      };
-    });
+  const btn = document.getElementById("btn");
+  const tam = document.getElementById("tam");
+  btn.addEventListener("click", function () {
+    tam.classList.toggle("show");
+
+    let laptop = window.matchMedia("(max-width: 601px)");
+    window.onresize = () => {
+      if (laptop.matches) {
+        tam.classList.toggle("show");
+      } else {
+        tam.classList.remove("show");
+      }
+    };
   });
-  
+});
